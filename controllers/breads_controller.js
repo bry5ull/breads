2e1cf36edba83dd02650a25acf31ffc2a2114165
bry5ull <bry@bry5ull.dev>
2022-05-15 13:02:01 -0500

    2nd

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,16 +1,24 @@
 const express = require('express')
-const breads = express.Router()
+const router = express.Router()
 const Bread = require('../models/bread.js')
 
 // INDEX
-breads.get('/', (req, res) => {
-  res.send(Bread)
+router.get('/', (req, res) => {
+    res.render('Index', {
+        breads: Bread,
+        title: 'Index Page'
+      }
+    )
+  // res.send(Bread)
 })
 
-module.exports = breads
 
-// SHOW
-breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })
+router.get('/:arrayIndex', (req, res) => {
+    const { arrayIndex } = req.params
+    res.send(Bread[arrayIndex])
+})
+
+module.exports = router
+
   
+
